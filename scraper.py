090f6ea515913e8739caf6c986b159d1b5dd0567
Thomas Bruno Almeida <nethomas10@hotmail.com>
2022-09-19 09:52:50 +0100

    decoupled file-saving function

--- scraper.py
+++ scraper.py
@@ -19,18 +19,20 @@ def main():
     extract_chapters(reddit.subreddit("manga").new(limit=100),new_chapters,watchlist)
     extract_chapters(reddit.subreddit("manga").hot(limit=25),new_chapters,watchlist)
 
+    save_to_file(new_chapters)
+
+def save_to_file(chapter_list):
     #record current time
     currdate = datetime.now().strftime("%Y%m%d%H%M%S")
 
     #save results to local file
-    if len(new_chapters) > 0:
+    if len(chapter_list) > 0:
         f2 = open(currdate+"_new_chapters.txt","wt")
-        for title in new_chapters:
+        for title in chapter_list:
            f2.write(title + "\n")
         f2.close()
     else:
         print("No results found")
-
 
 def extract_chapters(subreddit_view, collection, watchlist):
     for submission in subreddit_view:
